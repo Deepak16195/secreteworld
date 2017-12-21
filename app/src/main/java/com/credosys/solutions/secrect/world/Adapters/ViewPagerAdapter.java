c691fdd1ce5b85e089ffc9b17542440290b7b5d7
yogesh@credosyssolutions.com <mayur@credosys.in>
2017-12-21 19:57:24 +0530

    first upload

---
+++ app/src/main/java/com/credosys/solutions/secrect/world/Adapters/ViewPagerAdapter.java
@@ -0,0 +1,20 @@
+package com.credosys.solutions.secrect.world.Adapters;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+
+/**
+ * Created by win7 on 21-Dec-17.
+ */
+
+public class ViewPagerAdapter extends PagerAdapter{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return false;
+    }
+}
