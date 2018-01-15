5f66d3db246ffdf1cf43b26545c6660710c76746
yogesh@credosyssolutions.com <mayur@credosys.in>
2018-01-15 15:29:32 +0530

    update

--- app/src/main/java/com/credosys/solutions/secrete/world/fragments/SlideNavigation/AddContentFragment.java
+++ app/src/main/java/com/credosys/solutions/secrete/world/fragments/SlideNavigation/AddContentFragment.java
@@ -1,5 +1,6 @@
 package com.credosys.solutions.secrete.world.fragments.SlideNavigation;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,11 +8,15 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import com.credosys.solutions.secrete.world.MainActivity;
 import com.credosys.solutions.secrete.world.R;
+
 
 /**
  * Created by SONY on 14-01-18.
@@ -19,8 +24,11 @@ import com.credosys.solutions.secrete.world.R;
 
 public class AddContentFragment extends Fragment implements View.OnClickListener {
 
-    LinearLayout llTitle;
-    RelativeLayout rlTitleExpand;
+    LinearLayout llTitle,llMain,llCategory,llDesc,llExtdesc,llTag,llPic,rlPicExpand,llLocation;
+    RelativeLayout rlTitleExpand,rlCategoryExpand,rlDescExpand,rlExtdescExpand,rlTagExpand,rlLocationExpand;
+    ScrollView svMain;
+    Animation slide_down;
+//   ExpandableRelativeLayout rlTitleExpand;
     public static AddContentFragment newInstance() {
 
         Bundle args = new Bundle();
@@ -40,9 +48,46 @@ public class AddContentFragment extends Fragment implements View.OnClickListener
         ((MainActivity)getActivity()).setAppBarLayoutExpand(true,true);
         ((MainActivity)getActivity()).showBackButton(false);
 
+        llMain=v.findViewById(R.id.ll_main_addcontent);
         llTitle=v.findViewById(R.id.ll_addcontent_title);
-        llTitle.setOnClickListener(this);
         rlTitleExpand=v.findViewById(R.id.rl_title_expand);
+        svMain=v.findViewById(R.id.sv_main);
+        llCategory=v.findViewById(R.id.ll_addcontent_category);
+        rlCategoryExpand=v.findViewById(R.id.rl_category_expand);
+        llDesc=v.findViewById(R.id.ll_addcontent_description);
+        rlDescExpand=v.findViewById(R.id.rl_description_expand);
+        llExtdesc=v.findViewById(R.id.ll_addcontent_extdesc);
+        rlExtdescExpand=v.findViewById(R.id.rl_extdesc_expand);
+        llTag=v.findViewById(R.id.ll_addcontent_tag);
+        rlTagExpand=v.findViewById(R.id.rl_tag_expand);
+        llPic=v.findViewById(R.id.ll_addcontent_pic);
+        rlPicExpand=v.findViewById(R.id.rl_pic_expand);
+        llLocation=v.findViewById(R.id.ll_addcontent_location);
+        rlLocationExpand=v.findViewById(R.id.rl_location_expand);
+
+        llTitle.setOnClickListener(this);
+        llCategory.setOnClickListener(this);
+        llDesc.setOnClickListener(this);
+        llExtdesc.setOnClickListener(this);
+        llTag.setOnClickListener(this);
+        llPic.setOnClickListener(this);
+        llLocation.setOnClickListener(this);
+
+        slide_down = AnimationUtils.loadAnimation(getActivity(),
+                R.anim.slide_down);
+
+        Animation slide_up = AnimationUtils.loadAnimation(getActivity(),
+                R.anim.slide_up);
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            svMain.setNestedScrollingEnabled(true);
+
+        }
+
+
+
         return v;
     }
 
@@ -51,6 +96,24 @@ public class AddContentFragment extends Fragment implements View.OnClickListener
         switch (view.getId()){
             case R.id.ll_addcontent_title:
                 rlTitleExpand.setVisibility(View.VISIBLE);
+                break;
+            case R.id.ll_addcontent_category:
+                rlCategoryExpand.setVisibility(View.VISIBLE);
+                break;
+            case R.id.ll_addcontent_description:
+                rlDescExpand.setVisibility(View.VISIBLE);
+                break;
+            case R.id.ll_addcontent_extdesc:
+                rlExtdescExpand.setVisibility(View.VISIBLE);
+                break;
+            case R.id.ll_addcontent_tag:
+                rlTagExpand.setVisibility(View.VISIBLE);
+                break;
+            case R.id.ll_addcontent_pic:
+                rlPicExpand.setVisibility(View.VISIBLE);
+                break;
+            case R.id.ll_addcontent_location:
+                rlLocationExpand.setVisibility(View.VISIBLE);
                 break;
         }
     }
