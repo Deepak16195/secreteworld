3f0a5e6f9e1847742bd1cde8e946a9e2e672ca94
yogesh marathe <yogesh@credosyssolution.com>
2018-01-21 19:49:22 +0530

    split done

--- app/src/main/java/com/credosys/solutions/secrete/world/Adapters/NormalScroll/SplitAdapter.java
+++ app/src/main/java/com/credosys/solutions/secrete/world/Adapters/NormalScroll/SplitAdapter.java
@@ -27,52 +27,59 @@ import java.util.List;
 public class SplitAdapter extends RecyclerView.Adapter<SplitAdapter.SplitHolder> {
     private Context context;
     private List<Date> dateList;
-    private View inflate;
-    static int viewCount=0;
-    public SplitAdapter(Context context, List<Date> dateList,View inflate) {
-        this.context=context;
-        this.dateList=dateList;
-        this.inflate=inflate;
+
+    LinearLayout.LayoutParams params;
+
+    //    static int viewCount=0;
+    public SplitAdapter(Context context, List<Date> dateList) {
+        this.context = context;
+        this.dateList = dateList;
+
     }
 
     @Override
     public SplitHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.single_split_time,parent, false);
+                .inflate(R.layout.single_split_time, parent, false);
         return new SplitHolder(v);
     }
 
     @Override
     public void onBindViewHolder(SplitHolder holder, int position) {
-        final SplitHolder hold=holder;
+        final SplitHolder hold = holder;
+
+        params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         dateFormat.format(dateList.get(position));
         holder.txtPlanDate.setText(dateFormat.format(dateList.get(position)));
         holder.btnSplit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              hold.llSplitTimeList.addView(inflate,viewCount);
-              hold.llSplitTimeList.setId(viewCount);
-                viewCount++;
+                hold.llSplitTimeList.addView(addLayout());
             }
         });
     }
 
+    View addLayout(){
+        View inflate = LayoutInflater.from(context).inflate(R.layout.single_common_start_end_time, null, false);
+        return inflate;
+    }
     @Override
     public int getItemCount() {
         return dateList.size();
     }
 
 
-    class SplitHolder extends RecyclerView.ViewHolder{
+    class SplitHolder extends RecyclerView.ViewHolder {
         TextView txtPlanDate;
         Button btnSplit;
         LinearLayout llSplitTimeList;
-        public SplitHolder(View itemView) {
+
+        private SplitHolder(View itemView) {
             super(itemView);
-            txtPlanDate=itemView.findViewById(R.id.txt_plan_date);
-            btnSplit=itemView.findViewById(R.id.btn_split);
-            llSplitTimeList=itemView.findViewById(R.id.ll_split_time_list);
+            txtPlanDate = itemView.findViewById(R.id.txt_plan_date);
+            btnSplit = itemView.findViewById(R.id.btn_split);
+            llSplitTimeList = itemView.findViewById(R.id.ll_split_time_list);
 
         }
     }
