12f36d93986108807787a6612fcd8101421a267e
yogesh marathe <yogesh@credosyssolution.com>
2018-02-19 00:21:53 +0530

    some edited

--- app/src/main/java/com/credosys/solutions/secrete/world/fragments/SlideNavigation/SellerUserFragment.java
+++ app/src/main/java/com/credosys/solutions/secrete/world/fragments/SlideNavigation/SellerUserFragment.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.URLSpan;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
+import android.widget.TextView;
 
 import com.credosys.solutions.secrete.world.MainActivity;
 import com.credosys.solutions.secrete.world.R;
@@ -18,6 +24,8 @@ import com.credosys.solutions.secrete.world.R;
  */
 
 public class SellerUserFragment extends Fragment {
+
+    TextView txtPaypal;
     public static SellerUserFragment newInstance() {
 
         Bundle args = new Bundle();
@@ -38,6 +46,34 @@ public class SellerUserFragment extends Fragment {
         ViewStub viewStub = v.findViewById(R.id.vs_all);
         viewStub.setLayoutResource(R.layout.view_stub_seller_user);
         View inflated = viewStub.inflate();
+        txtPaypal=inflated.findViewById(R.id.txt_paypal);
+        SpannableStringBuilder stringBuilder=new SpannableStringBuilder("Do not have a PayPal account?\nOpen PayPal account in a few seconds");
+
         return v;
     }
+
+    protected void makeLinkClickable(SpannableStringBuilder strBuilder, final URLSpan span) {
+        int start = strBuilder.getSpanStart(span);
+        int end = strBuilder.getSpanEnd(span);
+        int flags = strBuilder.getSpanFlags(span);
+        ClickableSpan clickable = new ClickableSpan() {
+            public void onClick(View view) {
+                // Do something with span.getURL() to handle the link click...
+            }
+        };
+        strBuilder.setSpan(clickable, start, end, flags);
+        strBuilder.removeSpan(span);
+    }
+
+    protected void setTextViewHTML(TextView text, String html) {
+        CharSequence sequence = Html.fromHtml(html);
+        SpannableStringBuilder strBuilder = new SpannableStringBuilder(sequence);
+        URLSpan[] urls = strBuilder.getSpans(0, sequence.length(), URLSpan.class);
+        for(URLSpan span : urls) {
+            makeLinkClickable(strBuilder, span);
+        }
+        text.setText(strBuilder);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
 }
